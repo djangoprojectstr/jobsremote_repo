cbeede251471519383cc5765af4d1dde8d9d1736
djangoprojectstr <53656282+djangoprojectstr@users.noreply.github.com>
2019-09-09 09:35:39 +0530

    Update settings.py

--- matproject/matproject/settings.py
+++ matproject/matproject/settings.py
@@ -140,11 +140,11 @@ MEDIA_URL='/media/'
 
 #
 #
-MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER='python566775@gmail.com'
-EMAIL_HOST_PASSWORD='manush@143'
+# MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER='python566775@gmail.com'
+# EMAIL_HOST_PASSWORD='manush@143'
 # # EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 # # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
